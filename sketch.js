41fff7b7c620789c0317b00dfda53c2ba69bf8d8
rishit12345 <96435244+rishit12345@users.noreply.github.com>
2022-01-11 20:12:34 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,7 +39,7 @@ function draw() {
 }
 function keyPressed() {
 	if (keyCode===UP_ARROW){
-		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
 	}
 }
 
